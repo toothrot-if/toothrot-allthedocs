fa1549761b799ed9f235ab1277793a7da5332dc2
Jonathan Steinbeck <mail@steinbeck.it>
2017-12-29 02:35:50 +0100

    Add toothrot colorizer for story format

--- colorizer.js
+++ colorizer.js
@@ -1,23 +1,2 @@
 
-var loader = require("monaco-loader");
-var registerLanguage = require("toothrot-monarch").register;
-
-loader().then(function (monaco) {
-    
-    var codeElements = Array.prototype.slice.call(document.querySelectorAll("code.lang-toothrot"));
-    
-    registerLanguage(monaco);
-    
-    codeElements.forEach(function (element) {
-        
-        element.innerHTML = element.textContent;
-        
-        element.setAttribute("data-lang", "toothrot");
-        
-        monaco.editor.colorizeElement(element, {
-            theme: "toothrot",
-            tabSize: 4
-        });
-    });
-    
-});
+window.registerToothrotLanguage = require("toothrot-monarch").register;
